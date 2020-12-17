8732c616a436aee8d4671aec1fcfc575d61bd90f
Guilherme <guirlviana@gmail.com>
2020-12-17 16:17:16 -0300

    adicionado opcao de anexar arquivos

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+from os import read
 import PySimpleGUI as sg 
 
   
@@ -8,6 +9,10 @@ def layout():
             [sg.Text('Email para', tooltip='Separe por espaços ex (cont1@gmail.com cont2@gmail.com cont3@gmail.com)')],[sg.Input(size=(40,1), key="email_to", tooltip='Separe por espaços ex (cont1@gmail.com cont2@gmail.com cont3@gmail.com)')], 
             [sg.Text('Assunto')],[sg.Input(size=(40,1), key="assunto")],
             [sg.Text('Conteúdo do Email')],
-            [sg.Multiline(size=(40,8),autoscroll=True, key="conteudo")],
+            [sg.Multiline(size=(40,5),autoscroll=True, key="conteudo")],
+            
+            [sg.Text('Arquivos adicionados ao corpo do email: ')],
+            [sg.Multiline(size=(40,5), key='anexos',disabled=True)],
+            [sg.FilesBrowse('Anexar Arquivos',key='arquivos'),sg.Button('Visualizar anexos adicionados')],
             [sg.Button('Enviar email')]]
     return layout
--- main.py
+++ main.py
@@ -2,23 +2,73 @@ import PySimpleGUI as sg
 from config_email import Emailer  
 import gui 
 import threading
-
+import os
 sg.theme('DarkBrown3') 
-  
-
 layout = gui.layout()
+
 window = sg.Window('EMAIL MANAGER', layout) 
 
 def send_mail(values, window):
     window['Enviar email'].update(disabled=True)
     lista_contatos = values['email_to'].split()
     mail = Emailer(email_remetente=values['email_remetente'], senha_email=values['senha_email'])
+
     for contato in lista_contatos:
         mail.definir_conteudo(assunto=values['assunto'],
         lista_contatos=[contato], conteudo_email=values['conteudo'])
-        mail.enviar_email(intervalo=5, contato=contato)
+
+    lista_de_anexos = mostrar_arquivos(values=values)
+    lista_de_arquivos = formatar_somente_arquivos(lista=lista_de_anexos)
+    lista_de_imagens = formatar_somente_imagens(lista=lista_de_anexos)
+    # print(f'Lista de anexos: {lista_de_anexos}')
+    print('')
+    print(f'Lista de arquivos: {lista_de_arquivos}')
+    print('')
+    print(f'Lista de imagens: {lista_de_imagens}')
+        # mail.anexar_arquivos(lista_arquivos=lista_de_arquivos)
+        # mail.anexar_imagens(lista_imagens=lista_de_imagens)
+        # mail.enviar_email(intervalo=5, contato=contato)
     window.write_event_value('end up', 'emails enviados com sucesso!')
 
+def extract(caminho): 
+   str_pos = caminho.rfind("\\")
+   print(caminho[str_pos+1:])
+
+def mostrar_arquivos(values):
+    caminhos = values['arquivos'].replace(';', f'{os.linesep}')      
+    arquivos_formatados = caminhos.split()
+    lista_com_arquivos = []
+    for caminho in arquivos_formatados:
+        lista_com_arquivos.append(caminho)
+    
+    return lista_com_arquivos
+    
+
+def formatar_somente_arquivos(lista):
+    lista_formatada = []
+    if len(lista) == 0:
+        return lista_formatada
+    else:
+        for arquivo in lista:
+            if "png" in arquivo:
+                pass
+            elif "jpg" in arquivo:
+                pass
+            else:
+                lista_formatada.append(arquivo)
+        return lista_formatada
+        # diferenciar se é arquivo ou foto e mandar pra cada função (criar duas listas e mandar)
+def formatar_somente_imagens(lista):
+    lista_formatada = []
+    if len(lista) == 0:
+        return lista_formatada
+    else:
+        for arquivo in lista:
+            if "png" in arquivo:
+                lista_formatada.append(arquivo)
+            elif "jpg" in arquivo:
+                lista_formatada.append(arquivo)
+        return lista_formatada
 while True:   
     event, values = window.read() 
 
@@ -35,4 +85,12 @@ while True:
         print(values['end up'])
         window['Enviar email'].update(disabled=False)
 
+    if event == 'Visualizar anexos adicionados':
+        caminhos = mostrar_arquivos(values=values)
+        if len(caminhos) == 0:
+            window['anexos'].update('')
+        else:
+            window['anexos'].update(caminhos)
+        
+        
 window.close() 
